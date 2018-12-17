6bbe1c2e947bc6a1dbfe27304432a08b436e745e
李杰泓 <carolleesum@gmail.com>
2018-12-17 14:10:31 +0800

    serial asynchronous promise

--- Promise.js
+++ Promise.js
@@ -4,8 +4,8 @@ const REJECTED = 'rejected';
 
 function MyPromise(fn) {
   let self = this;
-  self.onFulfilled = null;
-  self.onRejected = null;
+  self.onFulfilledCallbacks = [];
+  self.onRejectedCallbacks = [];
   self.status = PENDING;
   // resolve的value
   self.value = null;
@@ -17,7 +17,9 @@ function MyPromise(fn) {
       setTimeout(()=>{
         self.status = FULFILLED;
         self.value = value;
-        self.onFulfilled(self.value);
+        self.onFulfilledCallbacks.forEach(callback => {
+          callback(self.value);
+        });
       })
     }
   }
@@ -27,7 +29,9 @@ function MyPromise(fn) {
       setTimeout(() => {
         self.status = REJECTED;
         self.error = error;
-        self.onRejected(self.error);
+        self.onRejectedCallbacks.forEach(callback => {
+          callback(self.error);
+        })
       })
     }
   }
@@ -35,16 +39,81 @@ function MyPromise(fn) {
 }
 
 MyPromise.prototype.then = function(onFulfilled, onRejected) {
-  if(this.status === PENDING) {
-    // 注册fulfilled和rejected对应的回调函数
-    this.onFulfilled = onFulfilled;
-    this.onRejected = onRejected;
-  } else if (this.status === FULFILLED) {
-    onFulfilled(this.value);
-  } else {
-    onRejected(this.error);
+  const self = this;
+  let bridgePromise; // 新的Promise对象，用以衔接后面的异步操作
+  // 提供默认回调函数
+  onFulfilled = typeof onFulfilled === 'function' ? onFulfilled : value => value;
+  onRejected = typeof onRejected === 'function' ? onRejected : error => { throw error };
+  
+  if(self.status === FULFILLED) {
+    return bridgePromise = new MyPromise((resolve, reject) => {
+      setTimeout(() => {
+        try {
+          let x = onFulfilled(self.value);
+          resolvePromise(bridgePromise, x, resolve, reject);
+        } catch (error) {
+          reject(error);
+        }
+      })
+    })
   }
-  return this;
+  if(self.status === REJECTED) {
+    return bridgePromise = new MyPromise((resolve, reject) => {
+      setTimeout(() => {
+        try {
+          let x = onRejected(self.error);
+          resolvePromise(bridgePromise, x, resolve, reject);
+        } catch (error) {
+          reject(error);
+        }
+      })
+    })
+  }
+  if(self.status === PENDING) {
+    // 为Promise添加成功回调和失败回调
+    return bridgePromise = new MyPromise((resolve, reject) => {
+      // resolve时会执行这个函数
+      self.onFulfilledCallbacks.push((value) => {
+        try {
+          let x = onFulfilled(value);
+          resolvePromise(bridgePromise, x, resolve, reject);
+        } catch (error) {
+          reject(error);
+        }
+      })
+      // reject时会执行这个函数
+      self.onRejectedCallbacks.push((error) => {
+        try {
+          let x = onRejected(error);
+          resolvePromise(bridgePromise, x, resolve, reject);
+        } catch (error) {
+          reject(error);
+        }
+      })
+    })
+  }
+}
+
+// 添加then方法，实际上就是只传onRejected不传onFulfilled的then方法
+MyPromise.prototype.catch = function (onRejected) {
+  return this.then(null, onRejected);
+}
+
+// 根据返回值x的类型来resolve该promise
+function resolvePromise(bridgePromise, x, resolve, reject) {
+  if(x instanceof MyPromise) {
+    if(x.status === PENDING) {
+      x.then(y => {
+        resolvePromise(bridgePromise,y,resolve,reject);
+      }, err => {
+        reject(err);
+      })
+    } else {
+      x.then(resolve, reject);
+    }
+  } else {
+    resolve(x);
+  }
 }
 
 module.exports = MyPromise;
--- test.js
+++ test.js
@@ -1,19 +1,43 @@
 const MyPromise = require('./Promise.js');
 
-let promise1 = new MyPromise((resolve, reject) => {
-  setTimeout(()=>{
-    console.log('1');
-    resolve(100);
-    console.log('2');
-  }, 0);
-}).then((value) => {
-  console.log(value);
-})
+// let promise1 = new MyPromise((resolve, reject) => {
+//   setTimeout(()=>{
+//     console.log('1');
+//     resolve(100);
+//     console.log('2');
+//   }, 0);
+// }).then((value) => {
+//   console.log(value);
+// })
 
-let promise2 = new MyPromise((resolve, reject) => {
-  resolve(200);
-}).then((value) => {
-  console.log(value);
-}).then((value)=> {
-  console.log(value * 2);
-})
+// let promise2 = new MyPromise((resolve, reject) => {
+//   resolve(200);
+// }).then((value) => {
+//   console.log(value);
+// }).then((value)=> {
+//   console.log(value * 2);
+// })
+
+let p = new MyPromise((resolve, reject) => {
+  resolve('p');
+});
+
+let f1 = function(data) {
+  console.log(data)
+  return new MyPromise((resolve, reject) => {
+      resolve('f1');
+  });
+}
+let f2 = function(data) {
+  console.log(data)
+  return new MyPromise((resolve, reject) => {
+      reject('err');
+  });
+}
+let f3 = function(data) {
+  console.log(data);
+}
+let errorLog = function(error) {
+  console.log(error)
+}
+p.then(f1).then(f2).then(f3).catch(errorLog)
